053c6c67e03fa5815edcc4706c4b742c3411039a
Long Hoang <hoangngoclong204@gmail.com>
2026-03-09 21:20:45 +0700

    Initial commit

---
+++ Backend/src/main/java/KLTN/RAG_CHATBOT_BE/RagChatbotBeApplication.java
@@ -0,0 +1,13 @@
+package KLTN.RAG_CHATBOT_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagChatbotBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagChatbotBeApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/KLTN/RAG_CHATBOT_BE/RagChatbotBeApplicationTests.java
@@ -0,0 +1,13 @@
+package KLTN.RAG_CHATBOT_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagChatbotBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
